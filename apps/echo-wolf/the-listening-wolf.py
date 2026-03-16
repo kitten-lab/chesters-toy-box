8332057474c2139bc31a6966288508e097f91d90
Dani 'Kat' Leve <the.danibobby@gmail.com>
2026-03-16 16:27:35 -0400

    chester made toy changes: 2026-03-16 16:27:35

--- apps/echo-wolf/the-listening-wolf.py
+++ apps/echo-wolf/the-listening-wolf.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 
 def initialize_db():
     conn = sqlite3.connect("eidn-wolf.db")
@@ -33,6 +34,26 @@ def count_messages():
     conn.close()
     return count
 
+def print_messages():
+
+    conn = sqlite3.connect("eidn-wolf.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT TEXT FROM messages (content)")
+    count = cursor.fetchone()[0]
+
+    conn.close()
+    return random.choice(count)
+
+
+write_actions = [
+    "The wolf writes it down.",
+    "The wolf scratches the words into the log.",
+    "The wolf tilts his head and records it.",
+    "The wolf listens carefully and stores the memory.",
+    "The wolf nods slowly and writes."
+]
+
 def run():
     print("The wires hum. The wolf leans forward.")
 
@@ -41,19 +62,23 @@ def run():
     while True:
         user_input = input("You: ")
 
-        if user_input.lower() == "goodbye":
+        if user_input.lower() == "end":
             print("The wolf dissolves back into the wires.")
             break
 
         if user_input.lower() == "help":
             print("Only you can help yourself.")
+        
+
+        if user_input.lower() == "$echo":
+            print_messages();
 
 
         save_message(user_input)
 
         total = count_messages()
 
-        print(f"The wolf writes it down. '{user_input}' is recorded to the memory.")
+        print(random.choice(write_actions) + f"\n'{user_input}' is recorded to the memory.")
         print(f"(Persistent memory size: {total})")
 
 if __name__ == "__main__":
